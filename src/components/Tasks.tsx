f3d6d1830fe955cbac65f6638212e772d9b5ac8e
Jorge de Souza J Junior <jorgedesouza@recrutae.com.br>
2022-08-13 21:08:08 -0300

    feat: task empty

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,3 +1,39 @@
+import PastIcon from "../assets/Icon/Past.icon";
+import styles from "./Tasks.module.css";
+import { Trash } from "phosphor-react"
+
 export const Tasks = () => {
-  return <h1>Hello world</h1>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.navTask}>
+        <div>
+          <h1>Tarefas criadas</h1>
+          <span>0</span>
+        </div>
+
+        <div>
+          <h1>Concluídas</h1>
+          <span>0</span>
+        </div>
+      </div>
+      <div className={styles.divider}></div>
+      {/* 
+      <div className={styles.empty}>
+        <PastIcon />
+        <h1>Você ainda não tem tarefas cadastradas</h1>
+        <h1>Crie tarefas e organize seus itens a fazer</h1>
+      </div> */}
+
+      <div className={styles.withFull}>
+        <input
+          type="checkbox"
+          id="newTask"
+          name=""
+          value=""
+        />
+        <label htmlFor="newTask"> Comprar geladeira </label>
+        <Trash size={20}/>
+      </div>
+    </div>
+  );
 };
