8027e33ab4aa85186de20cf902727a7915a637b6
Jorge de Souza J Junior <jorgedesouza@recrutae.com.br>
2022-10-08 19:22:58 -0300

    feat: add tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,72 @@
+import { useEffect, useState } from "react";
 import { Header } from "./components/Header";
-import { NewTask } from "./components/newTask";
 import { Tasks } from "./components/Tasks";
-import "./global.css";
+
+const LOCAL_STORAGE_KEY = "todo:savedTasks";
+
+export interface ITask {
+  id: string;
+  title: string;
+  isCompleted: boolean;
+}
+
 function App() {
+  const [tasks, setTasks] = useState<ITask[]>([]);
+
+  function loadSavedTasks() {
+    const saved = localStorage.getItem(LOCAL_STORAGE_KEY);
+    if (saved) {
+      setTasks(JSON.parse(saved));
+    }
+  }
+
+  useEffect(() => {
+    loadSavedTasks();
+  }, []);
+
+  function setTasksAndSave(newTasks: ITask[]) {
+    setTasks(newTasks);
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(newTasks));
+  }
+
+  function addTask(taskTitle: string) {
+    setTasksAndSave([
+      ...tasks,
+      {
+        id: crypto.randomUUID(),
+        title: taskTitle,
+        isCompleted: false,
+      },
+    ]);
+  }
+
+  function deleteTaskById(taskId: string) {
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+    setTasksAndSave(newTasks);
+  }
+
+  function toggleTaskCompletedById(taskId: string) {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        return {
+          ...task,
+          isCompleted: !task.isCompleted,
+        };
+      }
+      return task;
+    });
+    setTasksAndSave(newTasks);
+  }
+
   return (
-    <div className="App">
-      <Header />
-      <NewTask />
-      <Tasks />
-    </div>
+    <>
+      <Header onAddTask={addTask} />
+      <Tasks
+        tasks={tasks}
+        onDelete={deleteTaskById}
+        onComplete={toggleTaskCompletedById}
+      />
+    </>
   );
 }
 
